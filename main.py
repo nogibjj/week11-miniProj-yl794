c292fdf8b1b0b238018d12a7bf418f020bd1ba58
liuyunjia11 <105482610+liuyunjia11@users.noreply.github.com>
2023-10-18 20:07:57 +0000

    final-commit

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
-"""
-ETL-Query script
-"""
+import sqlite3
+import click
 
-from mylib.extract import extract
-from mylib.transform_load import load
-from mylib.query import query
+def Create(cursor):
+    # Create a table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL
+        )
+    """)
 
-# Extract
-print("Extracting data...")
-extract()
+def Update(cursor):
+    cursor.execute("INSERT INTO users (username) VALUES ('Lucy')")
+    cursor.execute("INSERT INTO users (username) VALUES ('Alice')")
 
-# Transform and load
-print("Transforming data...")
-load()
+def Delete(cursor):
+    cursor.execute("DELETE from users where username = 'Lucy'")
 
-# Query
-print("Querying data...")
-query()
+def Read(cursor):
+    cursor.execute("SELECT username FROM users")
+    users = cursor.fetchall()
+    return users
+
+@click.command()
+@click.argument("db")
+def command_line_tool(db):
+    '''
+        user guide for this command line tool
+    '''
+    click.echo("Welcome")
+    conn = sqlite3.connect("db")
+    cursor = conn.cursor()
+    Create(cursor)
+    Update(cursor)
+    Delete(cursor)
+    conn.commit()
+    users = Read(cursor)
+    for user in users:
+        click.echo(user)
+    
+    # Close the connection
+    conn.close()
+
+if __name__ == "__main__":
+    command_line_tool()
